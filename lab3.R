656e29b29cc27f5d40932c7f20575c2670c4be66
Alessia De Biase <alede379@student.liu.se>
2017-09-14 13:39:15 +0200

    Dijkstra algorithm

--- lab3.R
+++ lab3.R
@@ -13,14 +13,60 @@ euclidean<-function(a,b)
 
 
 dijkstra <- function(graph, init_node){
-  a<-length(graph[1])
-  b<-length(graph[2])
-  c<-length(graph[3])
-  if (is.data.frame(graph)==TRUE && is.numeric(init_node)==TRUE && is.vector(graph[1])==TRUE && is.vector(graph[2])==TRUE && is.vector(graph[3])==TRUE && a==b && b==c){
-    
-    
+  if (is.data.frame(graph)==TRUE && is.numeric(init_node)==TRUE && is.vector(graph[[1]])==TRUE && is.vector(graph[[2]])==TRUE && is.vector(graph[[3]])==TRUE){
+    v1<-graph[[1]]
+    v2<-graph[[2]]
+    w<-graph[[3]]
+    n<- #number of nodes
+    dist<-length(n) #distances
+    elem<-length(n) #element in the graph
+    elem[1]<-init_node
+    for(j in 1:n){
+      a<-Inf
+      for(i in 1:length(v1)){
+        if(v1[i]==init_node && w[i]<a){
+          a<-w[i] #save the weight
+          b<-i #save the position of the next node
+        }
+      }
+      elem[j]<-b
+      dist[j]<-a
+    }
   }else{
     stop()
   }
 }
 
+dijkstra <- function(graph, init_node){
+  if (is.data.frame(graph)==TRUE && is.numeric(init_node)==TRUE && is.vector(graph[[1]])==TRUE && is.vector(graph[[2]])==TRUE && is.vector(graph[[3]])==TRUE){
+    v1<-graph[[1]]
+    v2<-graph[[2]]
+    w<-graph[[3]]
+    n<- #number of nodes
+    dist<-length(n) #distances
+
+    for(i in 1:n){
+      if(init_node==i){
+        dist[i]=0
+      }else{
+        a<-Inf
+        b<-0
+        node<-init_node
+        while(v2[b]!=i){
+        for(j in 1:length(v1)){
+          if(v1[j]==node && w[j]<a){
+            a<-w[j] #save the weight
+            b<-j #save the position of the next node
+            node<-v2[j]
+              }
+          }
+        }
+        dist[i]<-a
+        }
+      }
+    }
+    
+  }else{
+    stop()
+  }
+}
