037a09f7102d915753c51a8856027d2b76d212ab
brbatv <barakatbruno@gmail.com>
2017-09-15 15:29:31 +0200

    basic documentation added

--- lab3ab/R/euclidean.R
+++ lab3ab/R/euclidean.R
@@ -1,6 +1,15 @@
+#' Find greatest common divisor between two numbers
+#' 
+#' @param a A number.
+#' @param b A number.
+#' @return Greatest common division using Euclidean's algorithm
+#' @examples
+#' euclidean(1000, 100)
+#' euclidean(10839893, 13238)
+
 euclidean <-
 function(a,b)
-{
+{ stopifnot(is.numeric (a) && is.numeric (b))
   a0=a # first element of sequence
   a1=b # second element
   while (a1!=0) 
