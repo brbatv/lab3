aa7958c8a00635514d07aeed80fc4a40d4ce3049
brbatv <barakatbruno@gmail.com>
2017-09-15 14:36:00 +0200

    added package without comments

---
+++ lab3ab/R/euclidean.R
@@ -0,0 +1,13 @@
+euclidean <-
+function(a,b)
+{
+  a0=a # first element of sequence
+  a1=b # second element
+  while (a1!=0) 
+  {
+    r=a0%%a1 # rest of euclidean division of a0 by a1
+    a0=a1 
+    a1=r
+  }
+  return(a0)
+}
