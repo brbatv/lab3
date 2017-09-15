2553f8e01fa22876116ac519effa13984aa40d8b
brbatv <barakatbruno@gmail.com>
2017-09-15 17:07:38 +0200

    added wiki_graph, its doc, and deleted wrong path

---
+++ R/wiki_graph.R
@@ -0,0 +1,12 @@
+#' Weighted graph with 6 vertices
+#'
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{v1}{Starting vertices of each edges}
+#'   \item{v2}{Ending vertices of these edges. }
+#'   \item{w}{Weights of each edge}
+#'   ...
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+"wiki_graph"
